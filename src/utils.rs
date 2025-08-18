911c1bd80592d0c9be0e079385f0d5232702b80c
Sax Dhiut <mail@iyxeyl.me>
2025-08-18 21:47:39 +0400

    chore: consolidate Clippy linting rules into Cargo.toml for improved code safety and maintainability

--- src/utils.rs
+++ src/utils.rs
@@ -1,24 +1,3 @@
-#![deny(
-    clippy::unwrap_used,
-    clippy::expect_used,
-    clippy::panic,
-    clippy::indexing_slicing,
-    clippy::unwrap_or_default,
-    clippy::get_unwrap,
-    clippy::map_unwrap_or,
-    clippy::unnecessary_unwrap,
-    clippy::missing_errors_doc,
-    clippy::missing_panics_doc,
-    clippy::todo,
-    clippy::unimplemented,
-    clippy::unreachable,
-    clippy::exit,
-    clippy::mem_forget,
-    clippy::clone_on_ref_ptr,
-    clippy::mutex_atomic,
-    clippy::rc_mutex
-)]
-
 use log::{debug, warn};
 use thiserror::Error;
 
